d650043cb4a723ff052bdf8f011ad580d9e82467
Felipe Gabriel de Carvalho <fg7318089@gmail.com>
2026-05-27 19:50:39 -0300

    Ajuste dos trem

---
+++ src/solucaoA.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Solução A" << std::endl;
+    return 0;
+}
---
+++ src/solucaoB.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Solução B" << std::endl;
+    return 0;
+}
