4936ac89ce56402185900dbc5615524c4d406db4
Thiago Gomes <1543790@sga.pucminas.br>
2026-05-29 11:51:17 -0300

    Criação do grafo da solução A

---
+++ src/include/mst.hpp
@@ -0,0 +1,39 @@
+#ifndef __MST_HPP__
+#define __MST_HPP__
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+class MST {
+    private:
+        float k;
+        Mat imagem;
+
+        struct Vertice {
+            int lin, col;
+            Vec3b cor; //RGB
+        };
+
+        struct Aresta {
+            int v1, v2;
+            int peso; //w
+
+            Aresta(int v1, int v2, int peso)
+            {
+                this->v1 = v1;
+                this->v2 = v2;
+                this->peso = peso;
+            }
+        };
+
+    public:
+        MST(string &imagemPath, float k);
+        int calcularPeso(Vertice v1, Vertice v2);
+        Mat segmentacao();
+};
+
+#endif // __MST_HPP__
--- src/solucaoA.cpp
+++ src/solucaoA.cpp
@@ -1,6 +1,63 @@
+#include "include/mst.hpp"
 #include <iostream>
+#include <filesystem>
+
+using namespace std;
+using namespace cv;
+
+MST::MST(string &imagemPath, float k){
+    imagem = imread(imagemPath, IMREAD_COLOR);
+
+    if(imagem.empty()){
+        cout << "ERRO AO CARREGAR IMAGEM \n" << endl;
+    }
+}
+
+Mat MST::segmentacao(){
+    int lin = imagem.rows;
+    int col = imagem.cols;
+    int numeroVertices = lin*col;
+
+    vector<Vertice> vertices(numeroVertices);
+    vector<Aresta> arestas;
+
+    // criação do grafo
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
+            vertices[i*col + j] = {i, j, imagem.at<Vec3b>(i, j)};
+        }
+    }
+
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
+
+            if(j < col - 1){
+                int peso = calcularPeso(vertices[i*col + j], vertices[i*col + (j+1)]);
+                arestas.push_back({i*col + j, i*col + (j+1), peso});
+            }
+            if(i < lin - 1){
+                int peso = calcularPeso(vertices[i*col + j], vertices[(i+1)*col + j]);
+                arestas.push_back({i*col + j, (i+1)*col + j, peso});
+            }
+        }
+    }
+
+    sort(arestas.begin(), arestas.end(), [](Aresta a, Aresta b) {
+        return a.peso < b.peso;
+    });
+
+    // união dos segmentos usando o algoritmo de Kruskal para encontrar a MST(sessão 4 do artigo)
+
+    return imagem; //por enquanto, depois tem que retornar a imagem segmentada
+}
+
+int MST::calcularPeso(Vertice v1, Vertice v2){
+    return abs(v1.cor[0] - v2.cor[0]) + abs(v1.cor[1] - v2.cor[1]) + abs(v1.cor[2] - v2.cor[2]);
+}
 
 int main() {
-    std::cout << "Solução A" << std::endl;
+    string imagemPath = "assets/images/predio.jpg";
+    MST mst(imagemPath, 0.5);
+    mst.segmentacao();
     return 0;
 }
